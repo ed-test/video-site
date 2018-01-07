0eb15613a70a15fd69f87d2931bdf1c3664afd19
Edwin Toh <edwin@qwertydesign.net>
2018-01-07 16:36:23 -0500

    setup(data): add mock data.

---
+++ src/mock-data.js
@@ -0,0 +1,74 @@
+const videos = [
+  {
+    id: '1',
+    title: 'Gold Rush',
+    thumbnail:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_1920,h_1082,x_0,y_0/c_fill,w_462,h_260,q_auto/v1515082810/159139.001v2.jpg',
+    image:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_3587,h_2019,x_414,y_329/c_fill,w_1400,h_788,q_auto/v1505836953/Gold-Rush-SN8-art-DISC.jpg',
+    genre: 'Documentary',
+    related: ['2'],
+  },
+  {
+    id: '2',
+    title: 'Killing Fields',
+    thumbnail:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_1920,h_1082,x_0,y_0/c_fill,w_462,h_260,q_auto/v1515082810/159139.001v2.jpg',
+    image:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_3587,h_2019,x_414,y_329/c_fill,w_1400,h_788,q_auto/v1505836953/Gold-Rush-SN8-art-DISC.jpg',
+    related: ['1'],
+    genre: 'Action',
+  },
+  {
+    id: '3',
+    title: 'Street Outlaws',
+    thumbnail:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_1920,h_1082,x_0,y_0/c_fill,w_462,h_260,q_auto/v1515082810/159139.001v2.jpg',
+    image:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_3587,h_2019,x_414,y_329/c_fill,w_1400,h_788,q_auto/v1505836953/Gold-Rush-SN8-art-DISC.jpg',
+    related: ['1'],
+    genre: 'Action',
+  },
+  {
+    id: '4',
+    title: 'The Interrogation Room',
+    thumbnail:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_1920,h_1082,x_0,y_0/c_fill,w_462,h_260,q_auto/v1515082810/159139.001v2.jpg',
+    image:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_3587,h_2019,x_414,y_329/c_fill,w_1400,h_788,q_auto/v1505836953/Gold-Rush-SN8-art-DISC.jpg',
+    related: ['1'],
+    genre: 'Documentary',
+  },
+  {
+    id: '5',
+    title: 'Moonshiners',
+    thumbnail:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_1920,h_1082,x_0,y_0/c_fill,w_462,h_260,q_auto/v1515082810/159139.001v2.jpg',
+    image:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_3587,h_2019,x_414,y_329/c_fill,w_1400,h_788,q_auto/v1505836953/Gold-Rush-SN8-art-DISC.jpg',
+    related: ['1'],
+    genre: 'Documentary',
+  },
+  {
+    id: '6',
+    title: 'Cash Cab',
+    thumbnail:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_1920,h_1082,x_0,y_0/c_fill,w_462,h_260,q_auto/v1515082810/159139.001v2.jpg',
+    image:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_3587,h_2019,x_414,y_329/c_fill,w_1400,h_788,q_auto/v1505836953/Gold-Rush-SN8-art-DISC.jpg',
+    related: ['1'],
+    genre: 'Documentary',
+  },
+  {
+    id: '7',
+    title: 'The Ballod of Parker and Todd',
+    thumbnail:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_1920,h_1082,x_0,y_0/c_fill,w_462,h_260,q_auto/v1515082810/159139.001v2.jpg',
+    image:
+      'https://res.cloudinary.com/discovery-go/image/upload/c_crop,w_3587,h_2019,x_414,y_329/c_fill,w_1400,h_788,q_auto/v1505836953/Gold-Rush-SN8-art-DISC.jpg',
+    related: ['1'],
+    genre: 'Action',
+  },
+];
+
+export { videos };
